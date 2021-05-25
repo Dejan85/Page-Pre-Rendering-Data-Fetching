6d2e8c61095afff033bc61bf713c92fe29b2beac
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-05-25 11:33:25 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
+import path from "path";
+import { promises } from "fs";
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data.json");
+
+  const jsonData = await promises.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
